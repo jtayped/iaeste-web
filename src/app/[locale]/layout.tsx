c1412ce325bd708d5f751b6faa115de9ecbfcdd5
jtayped <jtayped@gmail.com>
2024-11-16 14:54:00 +0100

    next-intl setup

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,40 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import type { Metadata } from "next";
+import "../globals.css";
+
+// TODO: add all other available metadata, and improve content for SEO
+export const metadata: Metadata = {
+  title: "IAESTE Lleida",
+  description:
+    "Organització sense ànim de lucre de l'Escola Politècnica Superior de la Universitat de Lleida.",
+};
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
