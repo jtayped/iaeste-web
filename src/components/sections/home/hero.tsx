7ad6466c0fded92847201bb9e8fb3499840318ce
jtayped <jtayped@gmail.com>
2024-11-19 10:58:01 +0100

    hero first draft

--- src/components/sections/home/hero.tsx
+++ src/components/sections/home/hero.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { BiMap } from "react-icons/bi";
@@ -13,12 +14,17 @@ const Hero = () => {
         alt="Hero background"
         className="absolute w-full h-full object-cover -z-10"
       />
-      <div className="h-[90vh] flex justify-center items-center bg-primary/50 text-white">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-8xl font-extrabold">{t("title")}</h1>
-          <div className="flex items-center gap-1 text-lg">
-            <BiMap />
-            <p>Lleida</p>
+      <div className="h-screen flex justify-center items-center bg-primary/60 text-white">
+        <div className="flex flex-col items-center text-center max-w-3xl">
+          <h1 className="text-8xl font-extrabold">{t("title")} LLEIDA</h1>
+          <p>{t("description")}</p>
+          <div className="flex items-center gap-3 mt-5">
+            <Button variant={"default"} size={"lg"}>
+              {t("buttons.student")}
+            </Button>
+            <Button variant={"secondary"} size={"lg"}>
+              {t("buttons.company")}
+            </Button>
           </div>
         </div>
       </div>
