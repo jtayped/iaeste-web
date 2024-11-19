a5f5b9b537101349943b3b4041cd6050bc8d7ef8
jtayped <jtayped@gmail.com>
2024-11-19 17:24:14 +0100

    collaborator section added

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,12 @@
 
+import Collaborators from "@/components/sections/collaborators";
 import Hero from "@/components/sections/home/hero";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <Collaborators/>
     </main>
   );
 }
