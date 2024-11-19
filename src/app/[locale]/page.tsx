ca9d0675c2e8f0d73170c5d500fd3482e8d31b19
jtayped <jtayped@gmail.com>
2024-11-19 17:59:09 +0100

    added about section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,13 @@
-
 import Collaborators from "@/components/sections/collaborators";
+import About from "@/components/sections/home/about";
 import Hero from "@/components/sections/home/hero";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
-      <Collaborators/>
+      <Collaborators />
+      <About />
     </main>
   );
 }
