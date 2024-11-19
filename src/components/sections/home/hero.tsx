4f619fe0c5e1620c2927f2d27a5d7681aac907ae
jtayped <jtayped@gmail.com>
2024-11-19 22:04:27 +0100

    removed unused import

--- src/components/sections/home/hero.tsx
+++ src/components/sections/home/hero.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-import { BiMap } from "react-icons/bi";
 
 const Hero = () => {
   const t = useTranslations("HomePage.hero");
