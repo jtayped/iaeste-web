4c9889cb7995afc309ead93e168dd1c8cf0f3ee2
jtayped <jtayped@gmail.com>
2024-11-19 17:59:37 +0100

    created collaborators constants

--- src/constants/collaborators.ts
+++ src/constants/collaborators.ts
@@ -1,10 +1,22 @@
 const collaborators = [
-  { src: "/collaborators/ntv.png", name: "NTV" },
-  { src: "/collaborators/teyme.png", name: "Teyme" },
-  { src: "/collaborators/diputacio.png", name: "Diputació de Lleida" },
-  { src: "/collaborators/udl.png", name: "Universitat de Lleida" },
-  { src: "/collaborators/ajuntament.png", name: "Ajuntament de Lleida" },
-  { src: "/collaborators/alumni.png", name: "Alumni de l'UdL" },
+  { src: "/collaborators/ntv.png", href: "https://ntv.es/", name: "NTV" },
+  {
+    src: "/collaborators/teyme.png",
+    href: "https://www.teyme.es/",
+    name: "Teyme",
+  },
+  {
+    src: "/collaborators/diputacio.png",
+    href: "",
+    name: "Diputació de Lleida",
+  },
+  { src: "/collaborators/udl.png", href: "", name: "Universitat de Lleida" },
+  {
+    src: "/collaborators/ajuntament.png",
+    href: "",
+    name: "Ajuntament de Lleida",
+  },
+  { src: "/collaborators/alumni.png", href: "", name: "Alumni de l'UdL" },
 ];
 
 export default collaborators;
