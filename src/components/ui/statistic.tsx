a2073223a26dcc717bfa59d73578e789e32a8ee7
jtayped <jtayped@gmail.com>
2024-11-20 10:34:47 +0100

    height fix

--- src/components/ui/statistic.tsx
+++ src/components/ui/statistic.tsx
@@ -11,14 +11,14 @@ const Statistic = ({
 }) => {
   const t = useTranslations("HomePage.stats");
   return (
-    <Card className="relative overflow-hidden">
+    <Card className="relative overflow-hidden h-full">
       <div className="absolute left-0 h-full w-1 bg-primary" />
-      <CardHeader>
+      <CardHeader className="pb-0 md:pb-4">
         <p className="text-4xl font-semibold text-primary">{stat}</p>
       </CardHeader>
       <CardContent>
-        <p className="text-xl">{t(`${translationKey}.title`)}</p>
-        <p className="text-sm text-muted-foreground">
+        <p className="text-lg md:text-xl">{t(`${translationKey}.title`)}</p>
+        <p className="text-xs md:text-sm text-muted-foreground">
           {t(`${translationKey}.subtitle`)}
         </p>
       </CardContent>
