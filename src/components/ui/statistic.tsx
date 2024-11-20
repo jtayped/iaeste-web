90f8e055ad87442a5a640517de686c1d923e6c62
jtayped <jtayped@gmail.com>
2024-11-20 15:19:38 +0100

    statistics counter animated

--- src/components/ui/statistic.tsx
+++ src/components/ui/statistic.tsx
@@ -1,20 +1,23 @@
 import { useTranslations } from "next-intl";
 import { Card, CardContent, CardHeader } from "./card";
 import React from "react";
+import AnimatedCounter from "./counter";
 
 const Statistic = ({
   translationKey,
   stat,
 }: {
   translationKey: string;
-  stat: string;
+  stat: number;
 }) => {
   const t = useTranslations("HomePage.stats");
   return (
     <Card className="relative overflow-hidden h-full">
       <div className="absolute left-0 h-full w-1 bg-primary" />
       <CardHeader className="pb-0 md:pb-4">
-        <p className="text-4xl font-semibold text-primary">{stat}</p>
+        <p className="text-4xl font-semibold text-primary">
+          <AnimatedCounter from={0} to={stat} duration={1} />+
+        </p>
       </CardHeader>
       <CardContent>
         <p className="text-lg md:text-xl">{t(`${translationKey}.title`)}</p>
