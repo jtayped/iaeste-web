0c4d8c7eaebf4ded7b14169e454190d4b97a8aa7
jtayped <jtayped@gmail.com>
2024-11-20 09:43:32 +0100

    hero mobile responsiveness

--- src/components/sections/home/hero.tsx
+++ src/components/sections/home/hero.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 const Hero = () => {
   const t = useTranslations("HomePage.hero");
+
   return (
     <div className="relative">
       <Image
@@ -13,10 +14,13 @@ const Hero = () => {
         alt="Hero background"
         className="absolute w-full h-full object-cover -z-10"
       />
-      <div className="h-screen flex justify-center items-center bg-primary/60 text-white">
+      <div className="h-screen flex justify-center items-center bg-primary/60 text-white px-screen">
         <div className="flex flex-col items-center text-center max-w-3xl">
-          <h1 className="text-8xl font-extrabold">{t("title")} LLEIDA</h1>
-          <p>{t("description")}</p>
+          <h1 className="text-6xl lg:text-8xl font-extrabold">
+            IAESTE
+            <span className="text-xl font-semibold">LLEIDA</span>
+          </h1>
+          <p className="text-sm md:text-md">{t("description")}</p>
           <div className="flex items-center gap-3 mt-5">
             <Button variant={"default"} size={"lg"}>
               {t("buttons.student")}
