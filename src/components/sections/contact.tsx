83932f9836f99e9094a5eb214d6c8ce2eae6fcf3
jtayped <jtayped@gmail.com>
2024-11-26 21:25:30 +0100

    social media icons changed and new mailto button added

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,23 +1,38 @@
 import { useTranslations } from "next-intl";
 import ContactForm from "../common/contact-form";
 import socials from "@/constants/socials";
-import Social from "../ui/social";
+import { Button } from "../ui/button";
+import { BiSend } from "react-icons/bi";
+import Link from "next/link";
 
 const Contact = () => {
   const t = useTranslations("contact");
-  
+
   return (
     <div className="px-screen grid md:grid-cols-2 gap-10 mt-14">
       <div>
-        <h2 className="text-3xl font-bold">{t("title")}</h2>
+        <div className="flex items-center gap-3.5">
+          <h2 className="text-3xl font-bold">{t("title")}</h2>
+          <ul className="flex items-center gap-2">
+            {socials.map((s) => {
+              const Icon = s.icon;
+              return (
+                <li key={s.name}>
+                  <Link href={s.href}>
+                    <Icon size={25} />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
         <p className="text-lg mt-3">{t("description")}</p>
-        <ul className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-5">
-          {socials.map((s) => (
-            <li key={s.name}>
-              <Social name={s.name} href={s.href} Icon={s.icon} />
-            </li>
-          ))}
-        </ul>
+        <Link href="mailto:iaeste@udl.cat">
+          <Button className="mt-5">
+            {t("btn")} <BiSend />
+          </Button>
+        </Link>
       </div>
       <ContactForm />
     </div>
