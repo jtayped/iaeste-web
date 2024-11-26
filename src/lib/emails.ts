db00aba558f226cac859c0edc4f4b7d73c13755f
jtayped <jtayped@gmail.com>
2024-11-26 19:54:28 +0100

    email sending fix

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -1,16 +1,18 @@
 "use server";
 
-import { Resend } from "resend";
-import formSchema from "@/validators/contact-form";
 import { z } from "zod";
+import { Resend } from "resend";
+import useFormSchema from "@/validators/contact-form";
 import { ContactFormEmail } from "@/emails/contact-form";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail(formValues: z.infer<typeof formSchema>) {
+export async function sendEmail(
+  formValues: z.infer<ReturnType<typeof useFormSchema>>
+) {
   const { email, name, subject, message } = formValues;
 
-  formSchema.parse(formValues)
+  // TODO: form validation
 
   await resend.emails.send({
     from: process.env.CONTACT_FORM_FROM as string,
