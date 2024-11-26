b920e7e773e00bc5df85ee561182d34973e115c2
jtayped <jtayped@gmail.com>
2024-11-26 20:12:16 +0100

    hover transition added

--- src/components/ui/social.tsx
+++ src/components/ui/social.tsx
@@ -13,7 +13,7 @@ const Social = ({
 }) => {
   return (
     <Link href={href}>
-      <Card className="flex flex-col items-center p-6">
+      <Card className="flex flex-col items-center p-6 hover:bg-gray-800/5 transition-colors">
         <Icon size={40} />
         <p className="mt-3 text-sm">{name}</p>
       </Card>
