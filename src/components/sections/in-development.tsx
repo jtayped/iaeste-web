dbb2a5e2d3fcf16851b86c639f6ab837a1924145
jtayped <jtayped@gmail.com>
2024-12-16 18:39:23 +0100

    in development placeholder page created

---
+++ src/components/sections/in-development.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@/i18n/routing";
+import React from "react";
+import { BiCode } from "react-icons/bi";
+import { Button } from "../ui/button";
+import BackButton from "../ui/back-btn";
+
+const InDevelopment = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <div className="flex flex-col items-center max-w-screen-sm px-screen">
+        <span className="p-4 bg-primary/30">
+          <BiCode size={40} />
+        </span>
+        <p className="text-3xl font-semibold mt-5">
+          This page is under construction!
+        </p>
+        <p className="mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, cum!
+        </p>
+        <div className="flex items-center gap-2 mt-4">
+          <Button asChild>
+            <Link href={"/"}>Home</Link>
+          </Button>
+          <BackButton variant="secondary">Go back</BackButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InDevelopment;
