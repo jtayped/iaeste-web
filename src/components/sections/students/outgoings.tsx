74652b849273806616f7d3cd6ee3eff2433f7b8f
jtayped <jtayped@gmail.com>
2025-02-17 22:41:38 +0100

    mobile responsiveness

--- src/components/sections/students/outgoings.tsx
+++ src/components/sections/students/outgoings.tsx
@@ -14,15 +14,15 @@ const Outgoings = () => {
 
   const Outgoing = ({ outgoing }: { outgoing: Outgoing }) => {
     return (
-      <Card className="w-[450px] h-[150px] flex gap-6 p-4">
+      <Card className="w-[200px] md:w-[450px] h-[300px] md:h-[150px] flex flex-col md:flex-row gap-6 p-4">
         <Image
           src={outgoing.image}
           alt={`${outgoing.name}'s internship`}
           width={300}
           height={400}
-          className="h-full object-cover w-[150px] shadow"
+          className="object-cover h-[100px] md:h-full w-full md:w-[150px] shadow"
         />
-        <article className="flex flex-col justify-between">
+        <article className="h-full md:h-auto flex flex-col justify-between">
           <div className="">
             <p className="text-xl">&quot;{t(`${outgoing.key}.quote`)}&quot;</p>
           </div>
