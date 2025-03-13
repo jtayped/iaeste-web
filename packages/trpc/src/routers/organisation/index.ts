11ffb5c5ea87850c6f444ea75780c26a85a82143
jtayped <jtayped@gmail.com>
2025-03-14 00:00:11 +0100

    unused vars

--- packages/trpc/src/routers/organisation/index.ts
+++ packages/trpc/src/routers/organisation/index.ts
@@ -1,9 +1,7 @@
-import { z } from "zod";
-
-import { createTRPCRouter, publicProcedure } from "@/trpc";
+import { createTRPCRouter } from "@/trpc";
 import { invitationRouter } from "./invitation";
 
 export const organisationRouter = createTRPCRouter({
     invitation: invitationRouter,
-    
+
 });
