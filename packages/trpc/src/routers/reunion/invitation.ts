fc302e51cead1d06289661b415bfe63516f95161
jtayped <jtayped@gmail.com>
2025-03-16 20:25:30 +0100

    added a reject function

--- packages/trpc/src/routers/reunion/invitation.ts
+++ packages/trpc/src/routers/reunion/invitation.ts
@@ -1,3 +1,21 @@
-import { createTRPCRouter } from "../../trpc";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../../trpc";
 
-export const invitationRouter = createTRPCRouter({});
+export const invitationRouter = createTRPCRouter({
+  reject: protectedProcedure
+    .input(
+      z.object({
+        excuse: z.string().min(3).max(999).optional(),
+        inviteId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input: { inviteId, excuse } }) => {
+      await Promise.all([
+        ctx.db.reunionInvitation.update({
+          where: { id: inviteId, participantId: ctx.session.user.id },
+          data: { willAssist: false, excuse },
+        }),
+        // TODO: send notification to admins from the group
+      ]);
+    }),
+});
