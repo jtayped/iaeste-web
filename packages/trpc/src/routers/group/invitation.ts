d72f20f9d2dc000125be6d442d9b8d6d3cb4110c
jtayped <jtayped@gmail.com>
2025-03-16 19:48:38 +0100

    basic group functionality

--- packages/trpc/src/routers/group/invitation.ts
+++ packages/trpc/src/routers/group/invitation.ts
@@ -1,3 +1,67 @@
-import { createTRPCRouter } from "../../trpc";
+import { z } from "zod";
+import {
+  adminProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+} from "../../trpc";
+import { GroupInviteState } from "@prisma/client";
 
-export const invitationRouter = createTRPCRouter({});
+export const invitationRouter = createTRPCRouter({
+  send: adminProcedure
+    .input(z.object({ groupId: z.string(), userId: z.string() }))
+    .mutation(async ({ ctx, input: { groupId, userId } }) => {
+      Promise.all([
+        ctx.db.groupInvite.create({
+          data: { userId, groupId, senderId: ctx.session.user.id },
+        }),
+
+        // TODO: send notification to user
+      ]);
+    }),
+  cancel: adminProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input: { id } }) => {
+      await ctx.db.groupInvite.update({
+        where: { id },
+        data: { state: GroupInviteState.CANCELLED },
+      });
+    }),
+  accept: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input: { id } }) => {
+      Promise.all([
+        ctx.db.groupInvite.update({
+          where: {
+            id,
+            userId: ctx.session.user.id,
+            state: GroupInviteState.PENDING,
+          },
+          data: {
+            state: GroupInviteState.ACCEPTED,
+            group: {
+              update: {
+                participants: { connect: { id: ctx.session.user.id } },
+              },
+            },
+          },
+        }),
+        // TODO: send notifications to group participants
+      ]);
+    }),
+  reject: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input: { id } }) => {
+      Promise.all([
+        ctx.db.groupInvite.update({
+          where: {
+            id,
+            userId: ctx.session.user.id,
+            state: GroupInviteState.PENDING,
+          },
+          data: { state: GroupInviteState.REJECTED },
+        }),
+
+        // TODO: send notification to group participants who are admins
+      ]);
+    }),
+});
