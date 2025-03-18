12c72884d0db9909a66beb6b57c2e87e63701d56
jtayped <jtayped@gmail.com>
2025-03-18 22:08:39 +0100

    using email package in main web app

--- apps/web/src/lib/emails.ts
+++ apps/web/src/lib/emails.ts
@@ -1,23 +1,16 @@
 "use server";
 
 import { type z } from "zod";
-import { Resend } from "resend";
 import type useFormSchema from "@/validators/contact-form";
-import { ContactFormEmail } from "@/emails/contact-form";
+import { sendEmail } from "@repo/email/resend";
+import ContactFormEmail from "@repo/email/contact-form";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function sendEmail(
-  formValues: z.infer<ReturnType<typeof useFormSchema>>
+export async function sendContactFormEmail(
+  values: z.infer<ReturnType<typeof useFormSchema>>
 ) {
-  const { email, name, subject, message } = formValues;
-
-  // TODO: form validation
-
-  await resend.emails.send({
-    from: process.env.CONTACT_FORM_FROM ?? "",
-    to: process.env.CONTACT_FORM_TO ?? "",
-    subject: `IAESTE WEB | ${subject}`,
-    react: await ContactFormEmail({ email, name, message }),
-  });
+  await sendEmail(
+    "iaeste.lleida@gmail.com",
+    `Formulari WEB | ${values.subject}`,
+    ContactFormEmail(values)
+  );
 }
