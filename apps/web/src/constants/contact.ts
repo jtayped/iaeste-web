c0c6332f553f4dd4567be58e1b71f90372b48133
jtayped <jtayped@gmail.com>
2025-03-21 21:02:34 +0100

    added contact constants

--- apps/web/src/constants/contact.ts
+++ apps/web/src/constants/contact.ts
@@ -1,1 +1,5 @@
-export const email = "iaeste.lleida@gmail.com"
+export const email = "iaeste@udl.cat";
+export const address =
+  "71, Jaume II Street (Office 3.06, Carrer de Pere de Cabrera, 3, 25001 Lleida";
+export const phone = "973 70 33 64";
+export const legalName = "IAESTE LC Lleida";
