0dad303fe4c87295f3bcbf61519175f703673ec8
jtayped <jtayped@gmail.com>
2025-03-22 22:44:35 +0100

    changed to catalan, removed apple signin and useless components

--- packages/ui/src/components/auth/signin/page.tsx
+++ packages/ui/src/components/auth/signin/page.tsx
@@ -3,7 +3,7 @@ import SigninForm from "./form";
 const SigninPageComponent = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+      <div className="w-full max-w-sm">
         <SigninForm />
       </div>
     </div>
