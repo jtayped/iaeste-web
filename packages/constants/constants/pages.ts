2d2b0fe57206880f911efa28d49d119c35a205f0
jtayped <jtayped@gmail.com>
2025-03-23 13:38:44 +0100

    package created with pages constants

---
+++ packages/constants/constants/pages.ts
@@ -0,0 +1,8 @@
+export const pages = {
+  signin: "/auth/signin",
+  register: "/auth/register",
+  invitation: "/auth/invitation",
+  requestPending: "/auth/request-pending",
+  resetPassword: "/auth/reset-password",
+  onboarding: "/onboarding",
+};
