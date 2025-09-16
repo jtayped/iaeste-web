f7339b687095e0ce4e6df78a9010026a11844000
jtayped <jtayped@gmail.com>
2025-09-16 15:19:39 +0200

    added hero and contact section

--- apps/web/src/app/[locale]/company/page.tsx
+++ apps/web/src/app/[locale]/company/page.tsx
@@ -1,9 +1,30 @@
-import InDevelopment from "@/components/sections/in-development";
+import ContactFormBtn from "@/components/common/buttons/contact-form";
+import SendEmailBtn from "@/components/common/buttons/send-email";
+import Content from "@/components/common/sections/content";
+import HeroSection from "@/components/common/sections/hero";
+import ContactSection from "@/components/sections/students/contact";
+import ButtonGroup from "@repo/ui/button-group";
+import { useTranslations } from "next-intl";
 
 const CompaniesPage = () => {
+  const t = useTranslations("CompanyPage.hero");
+
   return (
     <main>
-      <InDevelopment />
+      <HeroSection
+        title={t("title")}
+        description={t("description")}
+        backgroundImage="/team/ago-2024.webp"
+        component={
+          <ButtonGroup className="justify-center">
+            <ContactFormBtn />
+            <SendEmailBtn />
+          </ButtonGroup>
+        }
+      />
+      <Content>
+        <ContactSection />
+      </Content>
     </main>
   );
 };
---
+++ apps/web/src/components/sections/companies/hero.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompaniesHeroSection = () => {
+  return <div>CompaniesHeroSection</div>;
+};
+
+export default CompaniesHeroSection;
