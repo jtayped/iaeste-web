dec1cfbdc27bcc0ea9eda27a83292344d01be6c7
jtayped <jtayped@gmail.com>
2026-08-26 12:39:28 +0200

    mount keystatic in the web app (IA-55)

---
+++ apps/web/src/app/api/keystatic/[...params]/route.ts
@@ -0,0 +1,5 @@
+import { makeRouteHandler } from "@keystatic/next/route-handler";
+
+import keystaticConfig from "../../../../../../../keystatic.config";
+
+export const { GET, POST } = makeRouteHandler({ config: keystaticConfig });
